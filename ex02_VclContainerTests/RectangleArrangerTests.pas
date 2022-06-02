b979bfa1c27749a2eab0828e3a2488f1dc5958e3
Bogdan Polak <bogdanpol@gmail.com>
2022-06-02 22:42:45 +0200

    Updated and Refactored tests

--- ex02_VclContainerTests/RectangleArrangerTests.pas
+++ ex02_VclContainerTests/RectangleArrangerTests.pas
@@ -3,11 +3,16 @@
 interface
 
 uses
+  System.SysUtils,
+  System.StrUtils,
   DUnitX.TestFramework,
   Spring.Collections;
 
+{$M+}
+
 type
   TRectangle = class
+  public
     Left: Integer;
     Top: Integer;
     Width: Integer;
@@ -22,6 +27,7 @@ type
   end;
 
   TSettings = class
+  public
     MarginLeft: Integer;
     MarginTop: Integer;
     HorizontalSpace: Integer;
@@ -38,7 +44,7 @@ type
     constructor Create(const aSettings: TSettings);
     procedure Arrange(
       const aBackground: TRectangle;
-      var aRectangles: IReadOnlyList<TRectangle>);
+      aRectangles: IReadOnlyList<TRectangle>);
   end;
 
   [TestFixture]
@@ -46,15 +52,19 @@ type
   private
     sut: TRectangleArranger;
     fSettings: TSettings;
+    rectangles: IReadOnlyList<TRectangle>;
   public
     [Setup]
     procedure Setup;
     [TearDown]
     procedure TearDown;
   published
-    procedure GivenBackgroundLeftTopIsZero_ThenFirstWasArrenged;
-    procedure GivenBackgroundLeftTopIsNonZero_ThenFirstWasArrenged;
-    procedure GivenHavingTwoRectangles_ThenFirstAndLastWasArrenged;
+    procedure GivenBackgroundLeftTopAtZero_AndOneRectangle;
+    procedure GivenBackgroundLeftTopNonZero_AndOneRectangle;
+    procedure GivenTwoRectangles;
+    procedure GivenManyRectangles_ThenShouldWrap;
+    procedure GivenManyRectanglesWithDifferentSizes;
+    procedure GivenRectangles_ThenArragedOnesShouldNotBeMoved;
   end;
 
 implementation
@@ -84,19 +94,59 @@ end;
 
 procedure TRectangleArranger.Arrange(
   const aBackground: TRectangle;
-  var aRectangles: IReadOnlyList<TRectangle>);
+  aRectangles: IReadOnlyList<TRectangle>);
 var
   rectangle: TRectangle;
+  x: Integer;
+  y: Integer;
 begin
-  var x := aBackground.Left + fSettings.MarginLeft;
-  var y := aBackground.Top + fSettings.MarginTop;
-
+  x := aBackground.Left + fSettings.MarginLeft;
+  y := aBackground.Top + fSettings.MarginTop;
   for rectangle in aRectangles do
   begin
     rectangle.Left := x;
     rectangle.Top := y;
-    x := x + rectangle.Width - rectangle.Left + fSettings.HorizontalSpace;
+    x := x + rectangle.Width + fSettings.HorizontalSpace;
   end;
+end;
+
+// -----------------------------------------
+
+type
+  TRectangleHelper = class helper for TRectangle
+    procedure ShouldBe(
+      const rectangleName: string;
+      const aLeft: Integer;
+      const aTop: Integer;
+      const aWidth: Integer;
+      const aHeight: Integer);
+  end;
+  { TRectangleHelper }
+
+procedure TRectangleHelper.ShouldBe(
+  const rectangleName: string;
+  const aLeft, aTop, aWidth, aHeight: Integer);
+var
+  info: string;
+begin
+  if (aLeft <> self.Left) or (aTop <> self.Top) or (aWidth <> self.Width) or
+    (aHeight <> self.Height) then
+  begin
+    info := IfThen(rectangleName = '', 'rectangle', rectangleName);
+    if (aWidth = self.Width) and (aHeight = self.Height) then
+      Assert.Fail(Format('Expected %s position (%d,%d), but found (%d,%d)',
+        [info, aLeft, aTop, self.Left, self.Top]))
+    else
+      Assert.Fail
+        (Format('Expected %s (%d,%d) w:%d h:%d, but found (%d,%d) w:%d h:%d',
+        [info, aLeft, aTop, aWidth, aHeight, self.Left, self.Top, self.Width,
+        self.Height]))
+  end;
+end;
+
+function GivenRectangles(rects: TArray<TRectangle>): IReadOnlyList<TRectangle>;
+begin
+  Result := TCollections.CreateObjectList<TRectangle>(rects).AsReadOnlyList();
 end;
 
 // -----------------------------------------
@@ -119,61 +169,106 @@ begin
   sut.Free;
 end;
 
-procedure TRectangleArrangerTests.
-  GivenBackgroundLeftTopIsZero_ThenFirstWasArrenged;
+procedure TRectangleArrangerTests.GivenBackgroundLeftTopAtZero_AndOneRectangle;
 begin
   // Arrange
-  var
-  rectagles := TCollections.CreateList<TRectangle>
-    ([TRectangle.CreateA(0, 0, 50, 40)]).AsReadOnlyList();
+  rectangles := GivenRectangles([TRectangle.CreateA(0, 0, 50, 40)]);
 
   // Act
-  sut.Arrange(TRectangle.CreateA(0, 0, 900, 900), rectagles);
+  sut.Arrange(TRectangle.CreateA(0, 0, 900, 900), rectangles);
 
   // Assert
-  Assert.AreEqual(fSettings.MarginLeft, rectagles.First.Left);
-  Assert.AreEqual(fSettings.MarginTop, rectagles.First.Top);
-  Assert.AreEqual(50, rectagles.First.Width);
-  Assert.AreEqual(40, rectagles.First.Height);
+  rectangles.First.ShouldBe('rectagles.First', 10, 10, 50, 40);
+  Assert.Pass();
 end;
 
-procedure TRectangleArrangerTests.
-  GivenBackgroundLeftTopIsNonZero_ThenFirstWasArrenged;
+procedure TRectangleArrangerTests.GivenBackgroundLeftTopNonZero_AndOneRectangle;
 var
   rectangles: IReadOnlyList<TRectangle>;
 begin
   // Arrange
-  rectangles := TCollections.CreateObjectList<TRectangle>
-    ([TRectangle.CreateA(0, 0, 60, 40)]).AsReadOnlyList;
+  fSettings.MarginLeft := 20;
+  fSettings.MarginTop := 30;
+  rectangles := GivenRectangles([TRectangle.CreateA(0, 0, 60, 40)]);
 
   // Act
   sut.Arrange(TRectangle.CreateA(100, 200, 800, 600), rectangles);
 
   // Assert
-  Assert.AreEqual(fSettings.MarginLeft + 100, rectangles.First.Left);
-  Assert.AreEqual(fSettings.MarginTop + 200, rectangles.First.Top);
-  Assert.AreEqual(60, rectangles.First.Width);
-  Assert.AreEqual(40, rectangles.First.Height);
+  rectangles.First.ShouldBe('rectangles.First', 120, 230, 60, 40);
+  Assert.Pass();
+end;
+
+procedure TRectangleArrangerTests.GivenTwoRectangles;
+begin
+  // Arrange
+  rectangles := GivenRectangles([TRectangle.CreateA(0, 0, 65, 45),
+    TRectangle.CreateA(0, 0, 65, 45)]);
+
+  // Act
+  sut.Arrange(TRectangle.CreateA(100, 200, 800, 600), rectangles);
+
+  // Assert
+  rectangles.First.ShouldBe('rectangles.First', 110, 210, 65, 45);
+  rectangles.Last.ShouldBe('rectangles.Last', 185, 210, 65, 45);
+  Assert.Pass();
+end;
+
+procedure TRectangleArrangerTests.GivenManyRectangles_ThenShouldWrap;
+begin
+  // Arrange
+  rectangles := GivenRectangles([
+    { } TRectangle.CreateA(0, 0, 90, 45),
+    { } TRectangle.CreateA(0, 0, 90, 45),
+    { } TRectangle.CreateA(0, 0, 90, 45)]);
+
+  // Act
+  sut.Arrange(TRectangle.CreateA(0, 0, 250, 600), rectangles);
+
+  // Assert
+  rectangles.First.ShouldBe('rectangles.First', 10, 10, 90, 45);
+  rectangles[1].ShouldBe('rectangles[1]', 110, 10, 90, 45);
+  rectangles[2].ShouldBe('rectangles[2]', 10, 85, 90, 45);
+  Assert.Pass();
+end;
+
+procedure TRectangleArrangerTests.GivenManyRectanglesWithDifferentSizes;
+begin
+  // Arrange
+  rectangles := GivenRectangles([
+    { } TRectangle.CreateA(0, 0, 80, 30),
+    { } TRectangle.CreateA(1, 1, 120, 50),
+    { } TRectangle.CreateA(2, 2, 100, 40),
+    { } TRectangle.CreateA(3, 3, 90, 35)]);
+
+  // Act
+  sut.Arrange(TRectangle.CreateA(0, 0, 335, 600), rectangles);
+
+  // Assert
+  rectangles.First.ShouldBe('rectangles.First', 10, 10, 80, 30);
+  rectangles[1].ShouldBe('rectangles[1]', 100, 10, 120, 50);
+  rectangles[2].ShouldBe('rectangles[2]', 230, 10, 100, 40);
+  rectangles[3].ShouldBe('rectangles[3]', 10, 60, 90, 35);
+  Assert.Pass();
 end;
 
 procedure TRectangleArrangerTests.
-  GivenHavingTwoRectangles_ThenFirstAndLastWasArrenged;
+  GivenRectangles_ThenArragedOnesShouldNotBeMoved;
 begin
   // Arrange
-  var
-  rectagles := TCollections.CreateList<TRectangle>
-    ([TRectangle.CreateA(0, 0, 65, 45), TRectangle.CreateA(0, 0, 65, 45)])
-    .AsReadOnlyList();
+  rectangles := GivenRectangles([
+    { } TRectangle.CreateA(100, 10, 90, 40, true),
+    { } TRectangle.CreateA(1, 1, 90, 40, false),
+    { } TRectangle.CreateA(2, 2, 90, 40, false)]);
 
   // Act
-  sut.Arrange(TRectangle.CreateA(100, 200, 800, 600), rectagles);
+  sut.Arrange(TRectangle.CreateA(0, 0, 335, 600), rectangles);
 
   // Assert
-  Assert.AreEqual(fSettings.MarginLeft + 100, rectagles.First.Left);
-  Assert.AreEqual(fSettings.MarginTop + 200, rectagles.First.Top);
-
-  Assert.AreEqual(fSettings.MarginLeft + 100 + 60 + 10, rectagles.Last.Left);
-  Assert.AreEqual(fSettings.MarginTop + 200, rectagles.Last.Top);
+  rectangles.First.ShouldBe('rectangles.First', 100, 10, 90, 40);
+  rectangles[1].ShouldBe('rectangles[1]', 10, 10, 90, 40);
+  rectangles[2].ShouldBe('rectangles[2]', 200, 10, 90, 40);
+  Assert.Pass();
 end;
 
 initialization
